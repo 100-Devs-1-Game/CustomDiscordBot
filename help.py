55469e6b94e40d3787f434f88cd8b4616c111e14
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-29 03:34:13 +0200

    Add help "channel" sub command

--- help.py
+++ help.py
@@ -6,6 +6,21 @@ class Help(commands.Cog):
 	def __init__(self, bot: discord.Bot):
 		self.bot = bot
 
-	@discord.slash_command()
-	async def help(self, ctx: discord.ApplicationContext):
-		await ctx.respond("Not implemented yet!")
+	group = discord.SlashCommandGroup("help", "Help commands")	
+
+	@group.command()
+	async def channel(self, ctx: discord.ApplicationContext):
+		if isinstance(ctx.channel, discord.TextChannel):
+			desc = ctx.channel.topic or "No description set for this channel."
+			embed = discord.Embed(
+				title=f"#{ctx.channel.name}",
+				description=desc,
+				color=discord.Color.blue()
+			)
+		else:
+			embed = discord.Embed(
+				title="DM Channel",
+				description="Direct messages don't have a channel description.",
+				color=discord.Color.red()
+			)
+		await ctx.respond(embed=embed, ephemeral=True)
