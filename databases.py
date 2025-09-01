e7d2c7d888c045bbdb5c828c0e551139305dd5d2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-01 18:37:19 +0200

    Update databases.py

--- databases.py
+++ databases.py
@@ -1,17 +1,29 @@
 import sqlite3
 
 GAMES_DB = "dbs/games.db"
+TASKS_DB = "dbs/tasks.db"
+EVENTS_DB = "dbs/events.db"
 
 
 def add_game(name, repo_name, channel_id, owner):
-	conn = sqlite3.connect(GAMES_DB)
-	cursor = conn.cursor()
-	cursor.execute(
-		"INSERT INTO games (name, repo_name, channel_id, owner) VALUES (?, ?, ?, ?)",
-		(name, repo_name, channel_id, owner),
-	)
-	conn.commit()
-	conn.close()
+	insert_into_db(GAMES_DB, "games", name=name, repo_name=repo_name, channel_id=channel_id, owner=owner)	
+
+
+def add_task(user_id, description, deadline=None, event_id=None):
+	insert_into_db(TASKS_DB, "tasks", user_id=user_id, description=description, deadline=deadline, event_id=event_id)
+
+
+def insert_into_db(db_path, table, **columns):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    
+    keys = ', '.join(columns.keys())
+    placeholders = ', '.join(['?'] * len(columns))
+    values = tuple(columns.values())
+    
+    cursor.execute(f"INSERT INTO {table} ({keys}) VALUES ({placeholders})", values)
+    conn.commit()
+    conn.close()
 
 
 def setup_db(db_name, table_schemas):
@@ -24,3 +36,5 @@ def setup_db(db_name, table_schemas):
 
 
 setup_db(GAMES_DB, [{"name": "games", "columns": "id INTEGER PRIMARY KEY, name TEXT, repo_name TEXT, channel_id INTEGER, owner TEXT"}])
+setup_db(TASKS_DB, [{"name": "tasks", "columns": "id INTEGER PRIMARY KEY AUTOINCREMENT, user_id TEXT NOT NULL, description TEXT NOT NULL, deadline TEXT, finished INTEGER DEFAULT 0, event_id INTEGER DEFAULT NULL"}])
+setup_db(EVENTS_DB, [{"name": "events", "columns": "Id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, triggered INTEGER DEFAULT 0"}])
