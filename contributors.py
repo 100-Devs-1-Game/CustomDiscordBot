2d29f565fe6d05927b742b04b62232c8c9876a65
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-18 20:38:38 +0100

    Clarify command usage

--- contributors.py
+++ contributors.py
@@ -93,7 +93,7 @@ class Contributors(commands.Cog):
 
         if not contributor:
             await ctx.channel.send(
-                f"⚠️ {member.mention} please register as a contributor on our server ( `/contributors register` ) so you can be added.\nYou can react with ✅ to this message when you are done.",
+                f"⚠️ {member.mention} please register as a contributor on our server ( type `/contributors register` in the chat and press return ) so you can be added.\nYou can react with ✅ to this message when you are done.",
             )
 
             # not registered — tell owner how to get them registered
