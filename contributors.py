26ae42d8a9088d38384e676bf84e49c2bb55c050
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-26 08:29:48 +0100

    Fix trust level of unknown contributors with points

--- contributors.py
+++ contributors.py
@@ -652,7 +652,7 @@ class Contributors(commands.Cog):
 
         print(f"Trust score after trust points: {trust_score}")
 
-        if not unknown:
+        if not unknown or trust_points != 0:
             trust_score += 2  # start from medium if we have any trust points
 
         if not unknown or trust_score < 0:
