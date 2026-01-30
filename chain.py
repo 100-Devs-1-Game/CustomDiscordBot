5b7d5fc0cf42b3ff735bbad68ef6707cce84fef4
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-30 18:04:11 +0100

    Update chain.py

--- chain.py
+++ chain.py
@@ -1,10 +1,11 @@
+from datetime import datetime, timedelta
+
 import discord
 from discord.ext import commands
-from datetime import datetime, timedelta
 
 from utils import Utils
 
-SCHEDULE_CHANNEL_ID = 1427878178155659437  # Channel ID for Chain Game Collab schedule
+SCHEDULE_CHANNEL_ID = 1466537129583706305  # Channel ID for Chain Game Collab schedule
 SCHEDULE_DAYS = 8  # Total number of days in the schedule
 DATE_OFFSET = datetime(2026, 1, 31)  # Start date for the schedule (January 31, 2026)
 
@@ -15,9 +16,7 @@ class Chain(commands.Cog):
 
     group = discord.SlashCommandGroup("chain", "Chain Game Collab commands")
 
-    @group.command(
-        description="Reserve a day in the Chain Game Collab"
-    )
+    @group.command(description="Reserve a day in the Chain Game Collab")
     async def claim(self, ctx: discord.ApplicationContext, day: int):
         await ctx.defer(ephemeral=True)
 
@@ -31,18 +30,41 @@ class Chain(commands.Cog):
             break
 
         content = message.content.splitlines()
+        final_content = []
+        day_found = False
+        for line in content:
+            if line.startswith(f"*Day #{day}"):
+                day_found = True
+                if "Open" in line:
+                    reserver_name = ctx.author.mention
+                    final_content.append(
+                        Chain.get_day_str(day) + f"Reserved by {reserver_name}"
+                    )
+                else:
+                    await ctx.respond(
+                        f"Day #{day} is already reserved.", ephemeral=True
+                    )
+                    return
+            else:
+                final_content.append(line)
 
-         # Push the edit
-        await message.edit(content="\n".join(content))
+        if not day_found:
+            await ctx.respond(f"Day #{day} not found in the schedule.", ephemeral=True)
+            return
 
-        await ctx.respond("Schedule updated!", ephemeral=True)        
+        # Push the edit
+        await message.edit(content="\n".join(final_content))
+
+        await ctx.respond("Schedule updated!", ephemeral=True)
 
     @group.command(
         description="Update the schedule for the Chain Game Collab ( admin only )"
     )
-    async def updateschedule(self, ctx: discord.ApplicationContext, day: int):
+    async def updateschedule(self, ctx: discord.ApplicationContext):
         if not ctx.author.guild_permissions.manage_guild:
-            await ctx.respond("You do not have permission to use this command.", ephemeral=True)
+            await ctx.respond(
+                "You do not have permission to use this command.", ephemeral=True
+            )
             return
 
         await ctx.defer(ephemeral=True)
@@ -53,7 +75,9 @@ class Chain(commands.Cog):
 
         header_str = Chain.get_header_str()
 
+        empty = False
         if await Utils.channel_is_empty(schedule_channel):
+            empty = True
             await schedule_channel.send(header_str)
 
         # Get the very first message from the schedule channel
@@ -63,35 +87,52 @@ class Chain(commands.Cog):
 
         content = message.content.splitlines()
         final_content = []
-        # Ensure header exists
-        if not content or not content[0].startswith(header_str):
+        if empty:
             final_content = [header_str]
-            for i in SCHEDULE_DAYS:
-                timestamp = Chain.get_day_timestamp(i)
-                final_content.append(f"**Day #{i} ({timestamp}):** Open")
-        
+            for i in range(1, SCHEDULE_DAYS + 1):
+                final_content.append(Chain.get_day_str(i) + "Open")
+
         else:
-            remove_day = True
+            first_day = True
+            last_day_index = 0
+            add_day = False
 
             for line in content:
-                if remove_day and line.startswith("**Day"):
-                    remove_day = False
-                    continue  # skip this line to remove the reservation
+                if line.startswith("*Day"):
+                    if first_day:
+                        first_day = False
+                        timestamp_str = line.split(":")[1]
+                        date = datetime.fromtimestamp(int(timestamp_str))
+                        print("First day date:", date.isoformat())
+
+                        if date < datetime.now() - timedelta(days=1):
+                            add_day = True
+                            continue  # skip this line to remove the reservation
+                    last_day_index = line.split("#")[1].split("*")[0]
                 final_content.append(line)
+            if add_day:
+                final_content.append(
+                    Chain.get_day_str(int(last_day_index) + 1) + "Open"
+                )
 
         # Push the edit
         await message.edit(content="\n".join(final_content))
 
-        await ctx.respond("Schedule updated!", ephemeral=True)        
-
+        await ctx.respond("Schedule updated!", ephemeral=True)
 
     @staticmethod
     def get_header_str() -> str:
-        return "@silent**Reserved days:**"
+        return "**Reserved days:**\n"
 
     @staticmethod
     def get_day_timestamp(day: int) -> str:
         # Return a Discord timestamp string for a given day number with an 10:00 AM UTC time offset
         start_date = DATE_OFFSET
         day_date = start_date + timedelta(days=day - 1)
-        return day_date.strftime("<t:%s:T>") % int(day_date.replace(hour=10, minute=0, second=0, microsecond=0).timestamp())
+        return "<t:%s:d>" % int(
+            day_date.replace(hour=10, minute=0, second=0, microsecond=0).timestamp()
+        )
+
+    @staticmethod
+    def get_day_str(day: int) -> str:
+        return f"*Day #{day}* ({Chain.get_day_timestamp(day)}): "
